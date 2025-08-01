6355f27d5918fb6bd4f98dab12b9188e065be649
Ethan Evans <etevans2327@gmail.com>
2025-07-31 23:36:55 -0500

    got Apple stock data and graphed it

--- Main.py
+++ Main.py
@@ -10,3 +10,13 @@ import torch.optim as optim
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import root_mean_squared_error
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+ticker = 'AAPL'
+df = yf.download(ticker, '2020-01-01')
+df.Close.plot(figsize=(12, 8))
+plt.savefig('AAPL_Close.png')
+
+scaler = StandardScaler()
+df['Close'] = scaler.fit_transform(df['Close'])
+df.Close
