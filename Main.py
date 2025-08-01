fd8176e322e165435ed46074a965275643d96f91
Ethan Evans <etevans2327@gmail.com>
2025-07-31 23:56:14 -0500

    Added LSTM data preprocessing and initial PredictionModel class

--- Main.py
+++ Main.py
@@ -20,3 +20,37 @@ plt.savefig('AAPL_Close.png')
 scaler = StandardScaler()
 df['Close'] = scaler.fit_transform(df['Close'])
 df.Close
+
+seq_length = 30
+data = []
+
+for i in range(len(df) - seq_length):
+    data.append(df.Close[i:i+seq_length])
+
+data = np.array(data)
+
+train_size = int(0.8 * len(data))
+
+X_train = torch.from_numpy(data[:train_size, :-1, :])
+Y_train = torch.from_numpy(data[:train_size, -1, :])
+X_test = torch.from_numpy(data[train_size:, -1, :])
+Y_test = torch.from_numpy(data[train_size:, -1, :])
+
+class PredictionModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(PredictionModel, self).__init__()
+
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def foward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim, device=device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim, device=device)
+
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:, -1, :])
+
+        return out
